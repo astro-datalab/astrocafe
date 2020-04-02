43df62e6307727ac9e196a6ae52766fbdd18a866
Robert Nikutta <nikutta@noao.edu>
2020-04-02 00:02:58 -0600

    - Rename to astrodatalab/astrocafe - Update build.sh: exit if build step unsuccessful, check whether   docker logged in if --push option was given, implement docker push   command - Bump to 0.0.6 - Build (1.49 GB local) and push to Docker Hub for the first time   (534.51 MB compressed)

--- .docker/build.sh
+++ .docker/build.sh
@@ -35,14 +35,27 @@ while [ "$#" -gt 0 ]; do
     esac; shift
 done
 
+# if --push, check first if logged in to Docker Hub
+if [ $do_push == 1 ]; then
+    if [[ $(grep '"auths": {}' ~/.docker/config.json) ]]; then
+        echo "Not logged in to Docker Hub."
+        echo "Do 'docker login' first, then run this script again."
+        exit;
+    fi
+fi
+
 # build Docker image
 echo "Building Docker image $image:$tag ..."
 docker build -t $image:$tag .
+if [ $? != 0 ]; then
+    echo "docker build did not succeed. Aborting."
+    exit;
+fi
 
 # push image to Docker Hub
 if [ $do_push == 1 ]; then
     echo "Pushing image to Docker Hub..."
-    #docker push ...
+    docker push $image:$tag
 fi
     
 echo "Done."
